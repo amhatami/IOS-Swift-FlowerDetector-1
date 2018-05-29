ca01e8fcb04cfd0f66d6dda99304f96a58a05c22
Pooya Hatami <pooya@node-1w7jr9qrok5tbmfbapha9d96d.ipv6.telus.net>
2018-05-29 03:05:15 -0700

    done!

--- IOS-Swift-FlowerDetector/ViewController.swift
+++ IOS-Swift-FlowerDetector/ViewController.swift
@@ -10,16 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // MARK: Properties
+    let model = FlowerClassifier()
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var prediction: UILabel!
+    
+    // MARK: UIView
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setup(lhs: randRange(lower: 1, upper: 22))
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func setup(lhs:Int) {
+        
+        let flowername = "flower" + String(format:"%03d",lhs) + ".jpg"
+        
+        let file = flowername // "flower001.jpg" //"rose.jpg"
+        let image = UIImage(named: file)!
+        
+        let buffer = image.buffer()!
+        guard let output = try? model.prediction(data: buffer) else {
+            print("failed")
+            return
+        }
+        
+        let proba = Int(output.prob[output.classLabel]!*100)
+        
+        imageView.image = image
+        prediction.text = "I think it's a \(output.classLabel) at \(proba)%"
+        
     }
-
-
+    
+    func randRange (lower: Int , upper: Int) -> Int {
+        return lower + Int(arc4random_uniform(UInt32(upper - lower + 1)))
+    }
+    
+    
+    
+    @IBAction func nextBtn(_ sender: UIButton) {
+        
+        setup(lhs: randRange(lower: 1, upper: 22))
+        
+    }
+    
+    
+    
+    
 }
+
 
